fc3cf14c465c5fe10b0153cd76bc35b08253976b
Joshua Huang <jphuangjr@gmail.com>
2016-08-11 02:13:27 -0700

    brick count click function

--- public/client.js
+++ public/client.js
@@ -12,7 +12,7 @@ $("#count_box").on("click", function(){
     success: function(data){
       $("#main").hide();
       $("#list").show();
-      var html = "<div><h2 style='font-weight: bolder'>Signature List:</h2><span style='float: right' onclick='$(\"#main\").show(); $(\"list\").hide()'>CLOSE</span><br>"
+      var html = "<div><h2 style='font-weight: bolder'>Signature List:</h2><span style='float: right' onclick='$(\"#main\").show(); $(\"#list\").hide()'>CLOSE</span><br>"
       for(var i=0; i<data.length; i++){
         var title = data[i].message
         html += "<p>" + title + "</p>"
