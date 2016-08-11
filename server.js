9242b9b29505daa018d6e42ab5ddcfe3cc306c4a
Joshua Huang <jphuangjr@gmail.com>
2016-08-10 22:20:35 -0700

    moving stuff to server file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCDIPl1A2Q6tcg1ZHh8q6V8nWhXOssm5zI",
-  authDomain: "trumpwall-f504a.firebaseapp.com",
-  databaseURL: "https://trumpwall-f504a.firebaseio.com",
-  storageBucket: ""
-};
+//var config = {
+//  apiKey: "AIzaSyCDIPl1A2Q6tcg1ZHh8q6V8nWhXOssm5zI",
+//  authDomain: "trumpwall-f504a.firebaseapp.com",
+//  databaseURL: "https://trumpwall-f504a.firebaseio.com",
+//  storageBucket: ""
+//};
 
 //var app = firebase.initializeApp(config);
 //var database = app.database();
@@ -27,31 +27,6 @@ app.get("/", function (request, response) {
 
 app.get("/getBricks", function (request, response) {
 
-  //$.ajax({
-  //  url: 'https://api.mlab.com/api/1/databases/trump/collections/users?q={"email": "'+email+'"}&apiKey=mqvmM_b7JCNSRZg60uE18DljrstEwPuN',
-  //  type: "GET",
-  //  contentType: "application/json",
-  //  success: function(data){
-  //    if(data.length === 0){
-  //      $.ajax({
-  //        url: "https://api.mlab.com/api/1/databases/trump/collections/users?apiKey=mqvmM_b7JCNSRZg60uE18DljrstEwPuN",
-  //        data: JSON.stringify({email: email, name: name, message: title}),
-  //        type: "POST",
-  //        contentType: "application/json",
-  //        success: function(data){
-  //          console.log(data);
-  //          renderer()
-  //        },
-  //        error: function(xhr, status, err){
-  //          console.log(err);
-  //        }
-  //      });
-  //    }
-  //  },
-  //  error: function(xhr, status, err){
-  //    console.log(err);
-  //  }
-  //});
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
