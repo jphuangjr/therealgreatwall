c54bc315a95194c255a500fde50e75d45e036b9d
Joshua Huang <jphuangjr@gmail.com>
2016-08-10 17:56:10 -0700

    fixed crash?

--- server.js
+++ server.js
@@ -19,31 +19,31 @@ app.get("/", function (request, response) {
 
 app.get("/getBricks", function (request, response) {
 
-  $.ajax({
-    url: 'https://api.mlab.com/api/1/databases/trump/collections/users?q={"email": "'+email+'"}&apiKey=mqvmM_b7JCNSRZg60uE18DljrstEwPuN',
-    type: "GET",
-    contentType: "application/json",
-    success: function(data){
-      if(data.length === 0){
-        $.ajax({
-          url: "https://api.mlab.com/api/1/databases/trump/collections/users?apiKey=mqvmM_b7JCNSRZg60uE18DljrstEwPuN",
-          data: JSON.stringify({email: email, name: name, message: title}),
-          type: "POST",
-          contentType: "application/json",
-          success: function(data){
-            console.log(data);
-            renderer()
-          },
-          error: function(xhr, status, err){
-            console.log(err);
-          }
-        });
-      }
-    },
-    error: function(xhr, status, err){
-      console.log(err);
-    }
-  });
+  //$.ajax({
+  //  url: 'https://api.mlab.com/api/1/databases/trump/collections/users?q={"email": "'+email+'"}&apiKey=mqvmM_b7JCNSRZg60uE18DljrstEwPuN',
+  //  type: "GET",
+  //  contentType: "application/json",
+  //  success: function(data){
+  //    if(data.length === 0){
+  //      $.ajax({
+  //        url: "https://api.mlab.com/api/1/databases/trump/collections/users?apiKey=mqvmM_b7JCNSRZg60uE18DljrstEwPuN",
+  //        data: JSON.stringify({email: email, name: name, message: title}),
+  //        type: "POST",
+  //        contentType: "application/json",
+  //        success: function(data){
+  //          console.log(data);
+  //          renderer()
+  //        },
+  //        error: function(xhr, status, err){
+  //          console.log(err);
+  //        }
+  //      });
+  //    }
+  //  },
+  //  error: function(xhr, status, err){
+  //    console.log(err);
+  //  }
+  //});
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
@@ -59,11 +59,11 @@ var dreams = [
   "Wash the dishes"
   ];
 
-// listen for requests :)
-//var listener = app.listen(process.env.PORT, function () {
-//  console.log('Your app is listening on port ' + listener.address().port);
-//});
-
-var listener = app.listen(8005, function () {
-  console.log('Your app is listening on port ' + 8005);
+listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//var listener = app.listen(8005, function () {
+//  console.log('Your app is listening on port ' + 8005);
+//});
